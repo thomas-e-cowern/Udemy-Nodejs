b7fd1824114cdafc0c1e16c5f545a47b2c99c4de
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-10 06:03:38 -0500

    update index.js with path

--- App/index.js
+++ App/index.js
@@ -6,10 +6,24 @@
 
 // Dependencies
 var http = require('http')
+var url = require('url')
 
 // The server will respond with a string
 var server = http.createServer(function(req, res) {
-    res.end("Hello World\n")
+
+    // Get and parse url
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get path from url
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/\/+|\/+$/g,'')
+
+    // Send response
+    res.end("Hello World\n");
+
+    // Log path requested
+    console.log('Path: ' + path )
+    console.log('Trimmed Path: ' + trimmedPath )
 })
 
 // Start the server and listen at port 3000
