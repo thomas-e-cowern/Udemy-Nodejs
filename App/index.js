fc3a287bb29af1d53707476945748bef1f6a800e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-10 05:50:04 -0500

    update index.js with dependencies and server

--- App/index.js
+++ App/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+/*
+*   Primary file for API
+*   Created by Thomas Cowern
+*   10 December 2022
+*/
+
+// Dependencies
+var http = require('http')
+
+// The server will respond with a string
+var server = http.createServer(function(req, res) {
+    res.sendDate("Hellow World\n")
+})
+
+// Start the server and listen at port 3000
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000")
+})
