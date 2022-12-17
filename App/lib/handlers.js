2917a667ad67b37fa55600c267c62f6f492d5c09
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-17 09:06:00 -0500

    add varification for user input in handlers.js

--- App/lib/handlers.js
+++ App/lib/handlers.js
@@ -23,7 +23,12 @@ handlers._users = {};
 // required data: firstName, lastName, phone, password, tosAgreement
 // optional data: none
 handlers._users.post = function(data, callback) {
-
+    // check for required fields
+    var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+    var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    var tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false
 }
 
 // get users
